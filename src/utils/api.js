06f44a5035e9894a52e0ed4b4ea2163bdc9d54f9
Mohd Mustaqeem <mustakimraza38@gmail.com>
2026-03-14 17:37:42 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://postbackend-2-5t5a.onrender.com',
+  baseURL: 'https://posbackend-2-5t5a.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
